a416b039d5965063bc59da8be0ffd031708ac8ff
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-08-30 13:51:38 +0200

    setup el pollo loco

---
+++ classes/movable-object.class.js
@@ -0,0 +1,38 @@
+class MovableObject {
+    x = 80;
+    y = 280;
+    img;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.16;
+    otherDirection = false;
+
+
+    //height = 150;
+    //width = 100;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+        
+    }
+
+    moveRight() {
+        
+    }
+
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+
+    }
+}
